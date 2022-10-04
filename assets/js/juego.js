ed61c69b19b0d7cd9277c905ac8e6ea6c66c8481
Frank Tineo <frankfelix2016@outlook.es>
2022-10-04 14:39:44 -0400

    Clase 50 terminada

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,20 +9,34 @@ let deck = [];
 const types = ["C", "D", "H", "S"];
 const letter = ["A", "J", "Q", "K"];
 
+//Esta funcion crea un nuevo Deck o baraja
 const createDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let type of types) {
       deck.push(i + type);
     }
   }
+  //Adicionamos las las cartas especiales que tiene letras
   for (let i = 0; i < letter.length; i++) {
     for (let type of types) {
       deck.push(letter[i] + type);
     }
   }
-  console.log(deck);
-  deck = _.shuffle(deck);
+  //   console.log(deck);
+  deck = _.shuffle(deck); // con la libreria undescore barajamos el arreglo
   console.log(deck);
   return deck;
 };
 createDeck();
+
+//Esta funcion me permite tomar una carta
+const orderChart = () => {
+  if (deck.length === 0) {
+    throw "No hay Cartas en el Deck";
+  }
+  const chart = deck.pop();
+  console.log(deck);
+  console.log(chart); // carta deve de ser de la baraja
+  return chart;
+};
+orderChart();
